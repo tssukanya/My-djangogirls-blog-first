269a081e1066f72936b382745b20dafa1ec02e73
tssukanya <73799476+tssukanya@users.noreply.github.com>
2025-03-07 15:35:25 +0000

    Add first part tutorial

---
+++ python_intro.py
@@ -0,0 +1,16 @@
+print("Hello, Django girls!")
+print(4 * 4)
+if 3 > 2:
+    print("It works!")
+if 5 > 2:
+    print("5 is indeed greater than 2")
+else:
+    print("5 is not greater than 2")
+
+
+def hi():
+    print("Hi there!")
+    print("How are you?")
+
+
+hi()
